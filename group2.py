83e82c71a351d9ac72a4723b916c01dd803dcbca
Hassanabdelqader <43825916+Hassanabdelqader@users.noreply.github.com>
2018-10-19 02:12:08 +0300

    Add files via upload

---
+++ group2.py
@@ -0,0 +1,24 @@
+#number1
+def make_out_word(out, word):
+  return out[:2] + word + out[2:]
+
+#number2
+def without_end(str):
+  return str[1:-1]
+
+#numebr3
+def non_start(a, b):
+  return a[1:] + b[1:]
+
+#umber4
+def hello_name(name):
+  return 'Hello ' + name + '!'
+
+#numebr5
+def make_abba(a, b):
+  return a + b * 2 + a
+
+#number6
+def first_two(str):
+  return str[:2]
+
